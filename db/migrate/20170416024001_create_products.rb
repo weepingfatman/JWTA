a5619619eeb638ea61cdf9db3470fdab02980ecb
weepingfatman <davegee96.5@gmail.com>
2017-04-16 18:50:27 -0400

    added color field to products

---
+++ db/migrate/20170416024001_create_products.rb
@@ -0,0 +1,15 @@
+class CreateProducts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :products do |t|
+      t.string :year
+      t.string :make
+      t.string :model
+      t.string :miles
+      t.string :price
+      t.text :description
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
