92dea02a61e434f30d41733ae8a696135e6b51d1
weepingfatman <davegee96.5@gmail.com>
2017-04-16 20:03:53 -0400

    added orders

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :orders, only: [:index, :show, :create, :destroy]
+
 end
+
+
