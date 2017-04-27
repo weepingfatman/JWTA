2e86387eaf3b0eb0ed01d1d1985c50e8c5a9107a
weepingfatman <davegee96.5@gmail.com>
2017-04-26 22:36:51 -0400

    5.3 added contact form and mailer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,12 @@
 class StaticPagesController < ApplicationController
+
+ def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    UserMailer.contact_form(@email, @name, @message).deliver_now
+  end
+
   def landing_page
     @products = Product.limit(3)
   end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'dgould@jimwhites.com'
   layout 'mailer'
 end
