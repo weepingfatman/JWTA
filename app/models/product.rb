85cb70d69ab928852dc11a04b9087a6b92ff5081
weepingfatman <davegee96.5@gmail.com>
2017-04-28 22:03:29 -0400

    5.7 Added Ratings

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,20 @@
 class Product < ApplicationRecord
   has_many :orders
+  has_many :comments
+
   def self.search(search_term)
     Product.where("year LIKE ?", "%#{search_term}%")
-  end  
+  end 
+
+  def highest_rating_comment
+    comments.rating_desc.first
+  end 
+
+  def lowest_rating_comment
+    comments.rating_asc.first
+  end
+
+  def average_rating
+    comments.average(:rating).to_f
+  end
 end
