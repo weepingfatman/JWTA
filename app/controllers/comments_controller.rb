858d2e7f88b8a9c3b96152430e4bb6e0eaf37cca
weepingfatman <davegee96.5@gmail.com>
2017-04-30 20:46:25 -0400

    5.8 Added Star Rating System

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,14 @@
+class CommentsController < ApplicationController
+
+  def create
+    @product = Product.find(params[:product_id])
+    @comment = @product.comments.new(params[:comment])
+    @comment.user = current_user
+    @comment.save
+    redirect_to product_path(@product)
+  end
+
+  def destroy
+  end
+
+end
