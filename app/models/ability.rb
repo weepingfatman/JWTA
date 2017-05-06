8ed25127e09bd095c40bd4282bc7efc8e82783ce
weepingfatman <davegee96.5@gmail.com>
2017-05-05 23:07:55 -0400

    5.10 added admin restrictions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,9 +1,13 @@
+
 class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    user ||= User.new
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+    end
   end
-
 end
