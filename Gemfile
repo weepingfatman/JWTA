25e61ca8f29e9581c2492757c53ad8afb2e91f74
weepingfatman <davegee96.5@gmail.com>
2017-05-19 21:27:12 -0400

    6.1 fixed rspec

--- Gemfile
+++ Gemfile
@@ -69,4 +69,3 @@ gem 'will_paginate', '~> 3.1.0'
 
 gem 'rails-controller-testing'
 
-gem 'autoprefixer-rails'
--- spec/Models/product_spec.rb
+++ spec/Models/product_spec.rb
@@ -4,18 +4,20 @@ describe Product do
 
   before do
     @product = Product.create!(year: "2011", make: "Nissan", model: "Pathfinder")
+    @user = User.create!(email: "test@test.com", password: "testtest")
     @product.comments.create!(rating: 1, user: @user, body: "Awful SUV!")
     @product.comments.create!(rating: 3, user: @user, body: "OK SUV!")
     @product.comments.create!(rating: 5, user: @user, body: "Great SUV!")
   end
 
- it "returns the average rating of all comments"
- do
+  it "returns the average rating of all comments" do
+    expect(@product.average_rating).to eq 3
+  end
 
- expect(@product.average_rating).to eq(3)
+  it "is not valid" do
+    expect(Product.new(description: "Nice SUV")).not_to be_valid
+  end
  end
-end
-
 
 
 
