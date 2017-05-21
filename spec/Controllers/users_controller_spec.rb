76baf874bd38f241241d7515b9c7d0301da57833
weepingfatman <davegee96.5@gmail.com>
2017-05-20 20:21:23 -0400

    6.4 added factories

--- spec/Controllers/users_controller_spec.rb
+++ spec/Controllers/users_controller_spec.rb
@@ -5,24 +5,28 @@ describe UsersController, :type => :controller do
   let(:user) { FactoryGirl.create(:user)}
   let(:diffrent_user) { FactoryGirl.create(:user)}
 
-  describe 'get #show' do
-    context 'User is logged in' do
+  describe 'GET #show' do
+    context 'User is logged in' do 
       before do
-        sign_in @user
+        sign_in user
       end
-      it 'loads correct user details' do
-        get :show, id: @user.id
+
+      it "loads correct user details" do
+        get :show, params: {id: user.id}
         expect(response).to have_http_status(200)
-        expect(assigns(:user)).to eq @user
+        expect(assigns(:user)).to eq user
       end
-      it "can't access show page of the 2. user" do
-        get :show, id: @user2.id
+    end
+
+    context 'No user is logged in' do
+      it 'requires to login' do
+        get :show, params: {id: user.id}
         expect(response).to redirect_to(root_path)
       end
     end
     context 'No user is logged in' do
       it 'redirects to login' do
-        get :show, id: @user.id
+        get :show, params: {id: user.id}
         expect(response).to redirect_to(root_path)
       end
     end
