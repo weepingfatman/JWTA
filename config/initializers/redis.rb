6becbcca135718c3c14aea92522a0f4514deaf81
weepingfatman <davegee96.5@gmail.com>
2017-06-24 00:42:36 -0400

    6.15

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-  $redis = Redis.new(url: ENV["REDISTOGO_URL"])
+  $redis = Redis.new(url: ENV["REDIS_URL"])
 else
   $redis = Redis.new(host: 'localhost', port: 6379)
 end
