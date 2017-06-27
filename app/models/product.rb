8182c7bdeef31b355e9803cbad3705b02218ed25
weepingfatman <davegee96.5@gmail.com>
2017-06-26 21:39:42 -0400

    comments

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,9 +6,9 @@ class Product < ApplicationRecord
 
   def self.search(search_term)
     if Rails.env.development?
-      Product.where("name LIKE ?", "%#{search_term}%")
+      Product.where("make LIKE ?", "%#{search_term}%")
     else
-      Product.where("name ilike ?", "%#{search_term}%")
+      Product.where("make ilike ?", "%#{search_term}%")
     end
   end
 
